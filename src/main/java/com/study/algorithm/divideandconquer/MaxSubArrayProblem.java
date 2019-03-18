0241120a8babc38e9177f4722c601a4347ae6e53
asheremeta <asheremeta@degiro.nl>
2019-03-18 14:18:12 +0100

    max sub array problem

--- src/main/java/com/study/algorithm/divideandconquer/MaxSubArrayProblem.java
+++ src/main/java/com/study/algorithm/divideandconquer/MaxSubArrayProblem.java
@@ -1,5 +1,57 @@
 package com.study.algorithm.divideandconquer;
 
-public class MaximumSubArrayProblem {
+class MaxSubArrayProblem {
 
+    static int solve(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return divideAndConquer(0, array.length - 1, array);
+    }
+
+    private static int divideAndConquer(int start, int end, int[] array) {
+        if (start == end) {
+            return array[start];
+        }
+
+        int mid = (start + end) / 2;
+
+        int lMax = divideAndConquer(start, mid, array);
+        int rMax = divideAndConquer(mid + 1, end, array);
+
+        int cMax = crossMax(start, mid, end, array);
+
+        if (lMax >= rMax && lMax >= cMax) {
+            return lMax;
+        } else if (rMax >= lMax && rMax >= cMax) {
+            return rMax;
+        } else {
+            return cMax;
+        }
+    }
+
+    private static int crossMax(int start, int mid, int end, int[] array) {
+        int leftMax = 0, rightMax = 0;
+
+        int leftLocalMax = 0;
+        for (int i = mid; i >= start; i--) {
+            leftLocalMax = leftLocalMax + array[i];
+
+            if (leftLocalMax > leftMax) {
+                leftMax = leftLocalMax;
+            }
+        }
+
+        int rightLocalMax = 0;
+        for (int i = mid + 1; i <= end; i++) {
+            rightLocalMax = rightLocalMax + array[i];
+
+            if (rightLocalMax > rightMax) {
+                rightMax = rightLocalMax;
+            }
+        }
+
+        return leftMax + rightMax;
+    }
 }
--- src/test/java/com/study/algorithm/divideandconquer/MaxSubArrayProblemTest.java
+++ src/test/java/com/study/algorithm/divideandconquer/MaxSubArrayProblemTest.java
@@ -1,5 +1,13 @@
-import static org.junit.Assert.*;
+package com.study.algorithm.divideandconquer;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class MaxSubArrayProblemTest {
 
+    @Test
+    public void solve() {
+        assertThat(MaxSubArrayProblem.solve(new int[] { -1, 2, 3, 4, -10, 10, 12, -7, -9, 10 })).isEqualTo(22);
+    }
 }
