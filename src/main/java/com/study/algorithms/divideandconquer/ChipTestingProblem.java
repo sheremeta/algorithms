0fa94b71153b4b58c3dd42746706feacf2742b17
asheremeta <asheremeta@degiro.nl>
2019-03-21 10:23:34 +0100

    added Chip model

--- src/main/java/com/study/algorithms/divideandconquer/ChipTestingProblem.java
+++ src/main/java/com/study/algorithms/divideandconquer/ChipTestingProblem.java
@@ -1,51 +1,16 @@
 package com.study.algorithms.divideandconquer;
 
-import static java.lang.Math.random;
+import java.util.Collection;
 
 class ChipTestingProblem {
 
-    static Chip solve(Chip[] chips) {
+    static <T extends Chip> T solve(Collection<T> chips) {
+
         throw new IllegalStateException();
     }
 
-    public interface Chip {
+    public interface Chip<T extends Chip> {
 
-        static Chip good() {
-            return new GoodChip();
-        }
-
-        static Chip bad() {
-            return new BadChip();
-        }
-
-        boolean isGood();
-
-        boolean check(Chip chip);
-
-        class GoodChip implements Chip {
-
-            private GoodChip() {}
-
-            public boolean isGood() {
-                return true;
-            }
-
-            public boolean check(Chip chip) {
-                return chip.isGood();
-            }
-        }
-
-        class BadChip implements Chip {
-
-            private BadChip() {}
-
-            public boolean isGood() {
-                return false;
-            }
-
-            public boolean check(Chip chip) {
-                return random() < 0.5;
-            }
-        }
+        boolean check(T chip);
     }
 }
--- src/test/java/com/study/algorithms/divideandconquer/ChipTestingProblemTest.java
+++ src/test/java/com/study/algorithms/divideandconquer/ChipTestingProblemTest.java
@@ -3,16 +3,66 @@ package com.study.algorithms.divideandconquer;
 import org.junit.Test;
 
 import static com.study.algorithms.divideandconquer.ChipTestingProblem.Chip;
-import static com.study.algorithms.divideandconquer.ChipTestingProblem.Chip.bad;
-import static com.study.algorithms.divideandconquer.ChipTestingProblem.Chip.good;
+import static com.study.algorithms.divideandconquer.ChipTestingProblemTest.CheckableChip.bad;
+import static com.study.algorithms.divideandconquer.ChipTestingProblemTest.CheckableChip.good;
+import static java.lang.Math.random;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class ChipTestingProblemTest {
 
     @Test
     public void solve() {
-        Chip chip = ChipTestingProblem.solve(new Chip[] { bad(), bad(), good(), bad(), good(), good(), good() });
+        CheckableChip chip = ChipTestingProblem.solve(asList(bad(), bad(), good(), bad(), good(), good(), good()));
 
-        assertThat(chip.isGood()).isTrue();
+        assertThat(chip.state()).isTrue();
+    }
+
+    static class GoodChip extends CheckableChip {
+
+        private GoodChip() {
+            super(true);
+        }
+
+        public boolean check(CheckableChip chip) {
+            return chip.state();
+        }
+    }
+
+    static class BadChip extends CheckableChip {
+
+        private BadChip() {
+            super(false);
+        }
+
+        public boolean check(CheckableChip chip) {
+            return random() < 0.5;
+        }
+    }
+
+    abstract static class CheckableChip implements Chip<CheckableChip> {
+
+        private final boolean state;
+
+        static CheckableChip good() {
+            return new GoodChip();
+        }
+
+        static CheckableChip bad() {
+            return new BadChip();
+        }
+
+        CheckableChip(boolean state) {
+            this.state = state;
+        }
+
+        boolean state() {
+            return state;
+        }
+
+        @Override
+        public boolean check(CheckableChip chip) {
+            return state;
+        }
     }
 }
