5879f7b675253cf9bce24599453c3d7a3f4e42c8
asheremeta <asheremeta@degiro.nl>
2019-04-25 11:33:35 +0200

    changed MaxSubArrayProblemTest.java

--- src/test/java/com/study/algorithms/divideandconquer/MaxSubArrayProblemTest.java
+++ src/test/java/com/study/algorithms/divideandconquer/MaxSubArrayProblemTest.java
@@ -2,14 +2,19 @@ package com.study.algorithms.divideandconquer;
 
 import org.junit.Test;
 
+import static com.study.algorithms.util.Arrays.maximumSlice;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class MaxSubArrayProblemTest {
 
     @Test
     public void solve() {
-        int result = MaxSubArrayProblem.solve(new int[] { -1, 2, 3, 4, -10, 10, 12, -7, -9, 10 });
+        //O(n log n)
+        int result1 = MaxSubArrayProblem.solve(new int[] { -1, 2, 3, 4, -10, 10, 12, -7, -9, 10 });
 
-        assertThat(result).isEqualTo(22);
+        //O(n)
+        int result2 = maximumSlice(new int[] { -1, 2, 3, 4, -10, 10, 12, -7, -9, 10 });
+
+        assertThat(result1).isEqualTo(result2).isEqualTo(22);
     }
 }
