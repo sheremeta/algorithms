e5a20b1f527a21eab6ecbe3c71cf7566884bcc13
asheremeta <asheremeta@degiro.nl>
2019-04-26 16:18:55 +0200

    added islands evaluation

--- src/main/java/com/study/algorithms/util/Arrays.java
+++ src/main/java/com/study/algorithms/util/Arrays.java
@@ -1,5 +1,9 @@
 package com.study.algorithms.util;
 
+import java.util.Stack;
+
+import org.apache.commons.lang3.tuple.Pair;
+
 import static java.lang.Integer.max;
 
 public class Arrays {
@@ -14,5 +18,56 @@ public class Arrays {
         }
 
         return maximumSlice;
+    }
+
+    static int islands(int[][] arr, int rows, int cols) {
+        boolean[][] visited = new boolean[rows][cols];
+
+        int count = 0;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (!visited[row][col] && arr[row][col] == 1) {
+                    dfs(arr, row, col, visited, rows, cols);
+
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void dfs(int[][] arr, int row, int col, boolean[][] visited, int rows, int cols) {
+        Stack<Pair<Integer, Integer>> stack = new Stack<>();
+        stack.add(Pair.of(row, col));
+
+        while (!stack.isEmpty()) {
+            Pair<Integer, Integer> coordinate = stack.pop();
+
+            int currRow = coordinate.getLeft();
+            int currCol = coordinate.getRight();
+
+            visited[currRow][currCol] = true;
+
+            int[] rowStep = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] colStep = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+            for (int i = 0; i < 8; i++) {
+                int newRow = currRow + rowStep[i];
+                int newCol = currCol + colStep[i];
+
+                if (isSafe(arr, newRow, newCol, visited, rows, cols)) {
+                    stack.push(Pair.of(newRow, newCol));
+                }
+            }
+        }
+    }
+
+    private static boolean isSafe(int[][] arr, int row, int col, boolean[][] visited, int rows, int cols) {
+        return row >= 0 && row < rows
+            && col >= 0 && col < cols
+            && arr[row][col] == 1
+            && !visited[row][col];
     }
 }
--- src/test/java/com/study/algorithms/util/ArraysTest.java
+++ src/test/java/com/study/algorithms/util/ArraysTest.java
@@ -10,4 +10,29 @@ public class ArraysTest {
     public void maximumSlice() {
         assertThat(Arrays.maximumSlice(new int[] { 5, -7, 3, 5, -2, 4, -1 })).isEqualTo(10);
     }
+
+    @Test
+    public void islands() {
+        assertThat(Arrays.islands(new int[][] {
+            { 1, 1, 1, 0, 0, 0 },
+            { 1, 1, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 1, 1 },
+            { 0, 0, 0, 1, 1, 1 }
+        }, 5, 6)).isEqualTo(2);
+
+        assertThat(Arrays.islands(new int[][] {
+            { 1, 1, 1, 0, 0 },
+            { 1, 1, 1, 0, 0 },
+            { 0, 0, 1, 0, 0 },
+            { 0, 0, 0, 1, 1 },
+        }, 4, 5)).isEqualTo(1);
+
+        assertThat(Arrays.islands(new int[][] {
+            { 1, 1, 0, 1, 1 },
+            { 1, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 1 },
+        }, 4, 5)).isEqualTo(3);
+    }
 }
