37c764956cacbc1f589143e03b4b7fd0c0694032
asheremeta <asheremeta@degiro.nl>
2019-04-26 15:16:45 +0200

    changed CoinChangeProblem.java

--- src/main/java/com/study/algorithms/dynamicprogramming/CoinChangeProblem.java
+++ src/main/java/com/study/algorithms/dynamicprogramming/CoinChangeProblem.java
@@ -1,5 +1,11 @@
 package com.study.algorithms.dynamicprogramming;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.Integer.MAX_VALUE;
+
 class CoinChangeProblem {
 
     static int solve(int[] coins, int amount) {
@@ -16,5 +22,42 @@ class CoinChangeProblem {
         }
 
         return ways[amount];
+    }
+
+    static Collection<Integer> exchange(int coins[], int amount) {
+        int[] coinsPerAmount = new int[amount + 1];
+        coinsPerAmount[0] = 0;
+
+        int[] minCoinIndexes = new int[amount + 1];
+        minCoinIndexes[0] = 0;
+
+        for (int j = 1; j <= amount; j++) {
+            int minimum = MAX_VALUE;
+
+            int coin = 0;
+
+            for (int i = 1; i < coins.length; i++) {
+                if (j >= coins[i]) {
+                    if ((1 + coinsPerAmount[j - coins[i]]) < minimum) {
+                        minimum = 1 + coinsPerAmount[j - coins[i]];
+                        coin = i;
+                    }
+                }
+            }
+            coinsPerAmount[j] = minimum;
+            minCoinIndexes[j] = coin;
+        }
+
+        List<Integer> result = new LinkedList<>();
+
+        int tmpAmount = amount;
+        while (tmpAmount > 0) {
+            int coin = coins[minCoinIndexes[tmpAmount]];
+
+            result.add(coin);
+
+            tmpAmount = tmpAmount - coin;
+        }
+        return result;
     }
 }
--- src/test/java/com/study/algorithms/dynamicprogramming/CoinChangeProblemTest.java
+++ src/test/java/com/study/algorithms/dynamicprogramming/CoinChangeProblemTest.java
@@ -11,4 +11,9 @@ public class CoinChangeProblemTest {
         assertThat(CoinChangeProblem.solve(new int[] { 1, 5, 10 }, 12)).isEqualTo(4);
         assertThat(CoinChangeProblem.solve(new int[] { 1, 5, 10 }, 20)).isEqualTo(9);
     }
+
+    @Test
+    public void exchange() {
+        assertThat(CoinChangeProblem.exchange(new int[] { 0, 1, 5, 10 }, 36)).containsExactlyInAnyOrder(1, 5, 10, 10, 10);
+    }
 }
