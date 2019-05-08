7f2971e201a0edc459fbdc8e76b10557f37f1a4f
asheremeta <asheremeta@degiro.nl>
2019-05-08 10:50:23 +0200

    optimization

--- src/main/java/com/study/algorithms/leetcode/MaxSquareProblem.java
+++ src/main/java/com/study/algorithms/leetcode/MaxSquareProblem.java
@@ -1,8 +1,6 @@
 package com.study.algorithms.leetcode;
 
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-import static java.lang.StrictMath.pow;
+import static java.lang.Math.*;
 
 /*
 https://leetcode.com/problems/maximal-square/description/
@@ -10,43 +8,32 @@ https://leetcode.com/problems/maximal-square/description/
 class MaxSquareProblem {
 
     static int solve(int[][] matrix) {
-        int maxSquare = 0;
 
-        if (matrix.length == 0) {
-            return maxSquare;
+        int rows = matrix.length;
+
+        if (rows == 0) {
+            return 0;
         }
+        int columns = matrix[0].length;
 
-        int[] dp = new int[matrix[0].length];
+        int[][] squares = new int[rows + 1][columns + 1];
 
-        for (int[] row : matrix) {
-            for (int j = 0; j < row.length; j++) {
-                if (row[j] == 0 && dp[j] > 0) {
-                    maxSquare = max(maxSquare, maxSquare(dp));
+        int max = 0;
 
-                    dp[j] = 0;
-                } else {
-                    dp[j] = dp[j] + 1;
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (matrix[row][column] == 1) {
+                    int square = min(squares[row][column + 1], squares[row + 1][column]);
+
+                    square = min(square, squares[row][column]) + 1;
+
+                    squares[row + 1][column + 1] = square;
+
+                    max = max(square, max);
                 }
             }
         }
 
-        return (int) pow(max(maxSquare, maxSquare(dp)), 2);
-    }
-
-    private static int maxSquare(int[] dp) {
-        int max = 0, height = dp[0], weight = 1;
-
-        for (int i = 1; i < dp.length; i++) {
-            max = max(min(height, weight), max);
-
-            if (height == dp[i]) {
-                weight++;
-            } else {
-                height = dp[i];
-                weight = 1;
-            }
-        }
-
-        return max;
+        return max * max;
     }
 }
