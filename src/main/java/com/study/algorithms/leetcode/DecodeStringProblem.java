39175c1dc8d84bb42ebe638eb7039a7d1ac62868
asheremeta <asheremeta@degiro.nl>
2019-05-08 13:36:01 +0200

    added DecodeStringProblem.java

--- src/main/java/com/study/algorithms/leetcode/DecodeStringProblem.java
+++ src/main/java/com/study/algorithms/leetcode/DecodeStringProblem.java
@@ -1,11 +1,62 @@
 package com.study.algorithms.leetcode;
 
+import java.util.Stack;
+
+import static java.lang.Character.isDigit;
+
 /*
 https://leetcode.com/problems/decode-string/
  */
 class DecodeStringProblem {
 
     static String solve(String str) {
-        return "";
+        char[] chars = str.toCharArray();
+
+        String decoded = "";
+
+        Stack<Integer> nums = new Stack<>();
+        Stack<String> partialDecoded = new Stack<>();
+
+        int idx = 0;
+
+        while (idx < str.length()) {
+            if (isDigit(chars[idx])) {
+
+                StringBuilder digits = new StringBuilder();
+                digits.append(chars[idx]);
+
+                while (isDigit(chars[++idx])) {
+                    digits.append(chars[idx]);
+                }
+
+                nums.push(Integer.valueOf(digits.toString()));
+            } else if (chars[idx] == '[') {
+
+                partialDecoded.push(decoded);
+
+                decoded = "";
+
+                idx++;
+            } else if (chars[idx] == ']') {
+
+                int num = nums.pop();
+
+                StringBuilder tmp = new StringBuilder(partialDecoded.pop());
+                for (int i = 0; i < num; i++) {
+                    tmp.append(decoded);
+                }
+
+                decoded = tmp.toString();
+
+                idx++;
+            } else {
+
+                decoded = decoded + chars[idx];
+
+                idx++;
+            }
+        }
+
+        return decoded;
     }
 }
